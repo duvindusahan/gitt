4495c6f76de4662dafcc37ef938dd020eae8ce70
Duvindu Sahan <duvindusahan@gmail.com>
2024-06-25 09:49:08 +0530

    3

--- ExpenseTracker/home/models.py
+++ ExpenseTracker/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Income(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    source = models.CharField(max_length=255)
+    date = models.DateField()
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=255)
+    date = models.DateField()
+    description = models.TextField()
