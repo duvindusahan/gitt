d0d04777b9d88bcb9f4aff6b1ae33a2f2fafbce6
Duvindu Sahan <duvindusahan@gmail.com>
2024-06-25 09:51:51 +0530

    13

--- ExpenseTracker/home/views.py
+++ ExpenseTracker/home/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Income, Expense
+from .forms import IncomeForm, ExpenseForm
 
-# Create your views here.
+def dashboard(request):
+    incomes = Income.objects.filter(user=request.user)
+    expenses = Expense.objects.filter(user=request.user)
+    total_income = sum(income.amount for income in incomes)
+    total_expense = sum(expense.amount for expense in expenses)
+    return render(request, 'tracker/dashboard.html', {
+        'incomes': incomes,
+        'expenses': expenses,
+        'total_income': total_income,
+        'total_expense': total_expense
+    })
+
+def add_income(request):
+    if request.method == 'POST':
+        form = IncomeForm(request.POST)
+        if form.is_valid():
+            income = form.save(commit=False)
+            income.user = request.user
+            income.save()
+            return redirect('dashboard')
+    else:
+        form = IncomeForm()
+    return render(request, 'tracker/add_income.html', {'form': form})
+
+def add_expense(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            expense = form.save(commit=False)
+            expense.user = request.user
+            expense.save()
+            return redirect('dashboard')
+    else:
+        form = ExpenseForm()
+    return render(request, 'tracker/add_expense.html', {'form': form})
